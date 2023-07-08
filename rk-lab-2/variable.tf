1d9362cdcac2022cad7f2686520a0899bfbe6291
raihan1khan <raihan1khan@gmail.com>
2023-07-08 08:56:46 -0400

    adding storage

---
+++ rk-lab-2/variable.tf
@@ -0,0 +1,24 @@
+# variable "ami" {
+#     description = "Define your AMI id"
+#     type = string
+#     default = "ami-0e820afa569e84cc1"
+# }
+
+# variable "instance_type" {
+
+#     description = "what type of instance"
+#     type = string
+# }
+
+# # variable "tags" {
+
+# #     description = "Define the tags"
+# #     type = map(string)
+# # }
+
+
+# variable "region" {
+#   description = "The AWS region for the resources"
+#   type        = string
+#   default     = "us-east-1"
+# }
